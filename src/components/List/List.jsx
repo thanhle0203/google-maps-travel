68962b821845f5cdf35d56f2c9c9718b13fac3e9
Thanh Le <thanhlevan0203@gmail.com>
2022-10-28 22:19:48 -0700

    Create List Component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core'
+
+import useStyles from './styles'
 
 const List = () => {
+  const classes = useStyles();
+
   return (
-    <div>List</div>
+    <div className={classes.container}>
+      <Typography variant='h4'>Restaurants, Hotels & Attractions around you</Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={} onChange={}>
+          <MenuItem value='restaurants'>Restaurants</MenuItem>
+          <MenuItem value='hotels'>Hotels</MenuItem>
+          <MenuItem value='attractions'>Attractions</MenuItem>
+        </Select>
+
+      </FormControl>
+    </div>
   )
 }
 
