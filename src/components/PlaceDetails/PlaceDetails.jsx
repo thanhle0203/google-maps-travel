fc0c4ee753eb2931dd2909c14a9f428ed8e3928f
Thanh Le <thanhlevan0203@gmail.com>
2022-10-30 00:32:57 -0700

    Fetch Detail Restaurant Info

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Typography, Button, Card, CardMedia, CardContent, CartActions, Chip } from '@material-ui/core'
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core'
 import LocationOnIcon from '@material-ui/icons/LocationOn'
 import PhoneIcon from '@material-ui/icons/Phone'
 import Rating from '@material-ui/lab/Rating'
@@ -37,6 +37,25 @@ const PlaceDetails = ({ place }) => {
         {place?.cuisine.map(({ name }) => (
           <Chip key={name} size="small" label={name} className={classes.chip} />
         ))}
+        {place?.address && (
+          <Typography gutterBottom variant='subtitle2' color='textSecondary' className='subtitle'>
+            <LocationOnIcon /> {place.address}
+          </Typography>
+        )}
+        {place?.phone && (
+          <Typography gutterBottom variant='subtitle2' color='textSecondary' className='spacing'>
+            <PhoneIcon /> {place.phone}
+          </Typography>
+        )}
+        <CardActions>
+          <Button size='small' color='primary' onClick={() => window.open(place.web_url, '_blank')}>
+            Trip Advisor
+          </Button>
+
+          <Button size='small' color='primary' onClick={() => window.open(place.website, '_blank')}>
+            Website
+          </Button>
+        </CardActions>
 
       </CardContent>
 
