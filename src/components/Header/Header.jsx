f8bcb018cc9ac12e10e6f2b6d5acda520971138b
Thanh Le <thanhlevan0203@gmail.com>
2022-11-03 20:35:52 -0700

    Create Search Place API

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import SearchIcon from '@material-ui/icons/Search'
 
 import useStyles from './styles'
 
-const Header = () => {
+const Header = ({ setCoordinates }) => {
   const classes = useStyles();
   const [autocomplete, setAutocomplete] = useState(null);
 
@@ -13,6 +13,10 @@ const Header = () => {
 
   const onPlaceChanged = () => {
     const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+
+    setCoordinates({ lat, lng });
+
   }
 
   return (
@@ -25,7 +29,7 @@ const Header = () => {
           <Typography variant='h6' className={classes.title}>
             Explore new places
           </Typography> 
-            <Autocomplete onLoad={} onPlaceChanged={}> 
+            <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}> 
               <div className={classes.search}>
                 <div className={classes.searchIcon}>
                   <SearchIcon />
